308c6156824a49dc4454688feee54f12c9760f65
Daniel Coombs <danielcoombs005@yahoo.com>
2019-08-16 15:15:04 -0400

    experimenting with pokemon attributes

--- pokemontradingreactapp/src/components/PokemonLogic.js
+++ pokemontradingreactapp/src/components/PokemonLogic.js
@@ -2,11 +2,18 @@ import React from 'react';
 import '../App.css';
 
 class PokemonLogic extends React.Component {
+    costructor() {
+
+    }
+
+
     render() {
         return (
             <div>
                 <div className = "pokeinfo">
-                    
+                    <h3 id="name">Name</h3>
+                    <h3 id="desc">Description</h3>
+                    <h3 id="pkmnid">Pokemon ID</h3>
                 </div>
             </div>
         )
