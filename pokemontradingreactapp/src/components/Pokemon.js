7aaace93c43cd56e1f088963a7bd59d2df3a6c0b
Daniel Coombs <danielcoombs005@yahoo.com>
2019-08-20 16:41:57 -0400

    reverted back to hard code, to do

--- pokemontradingreactapp/src/components/Pokemon.js
+++ pokemontradingreactapp/src/components/Pokemon.js
@@ -6,37 +6,68 @@ class Pokemon extends React.Component {
     constructor() {
         super();
         this.state = {
-            pokeArray: []
+            poke1: {},
+            poke2: {},
+            poke3: {},
+            poke4: {},
+            poke5: {},
+            poke6: {},
+            poke7: {},
+            poke8: {},
+            poke9: {}
         }
+        this.fetchData = this.fetchData.bind(this);
     }
 
-    async componentDidMount() {
-        const tempArray = []
-        for (let i = 0; i < 9; i++) {
-            tempArray.push(
-                fetch("https://localhost:5001/api/values")
-                    .then(response => response.json())
-            )
-        }
-        await Promise.all(tempArray)
-            .then(data => {
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = ()  => {
+        Promise.all([
+            fetch("https://localhost:5001/api/values"),
+            fetch("https://localhost:5001/api/values"),
+            fetch("https://localhost:5001/api/values"),
+            fetch("https://localhost:5001/api/values"),
+            fetch("https://localhost:5001/api/values"),
+            fetch("https://localhost:5001/api/values"),
+            fetch("https://localhost:5001/api/values"),
+            fetch("https://localhost:5001/api/values"),
+            fetch("https://localhost:5001/api/values")
+        ])
+            .then(([res1, res2, res3, res4, res5, res6, res7, res8, res9]) =>
+                Promise.all([res1.json(), res2.json(), res3.json(),
+                             res4.json(), res5.json(), res6.json(),
+                             res7.json(), res8.json(), res9.json()]))
+            .then(([data1, data2, data3, data4, data5, data6, data7, data8, data9]) =>
                 this.setState({
-                    pokeArray: tempArray
-                });
-            })
-            .then(() => this.render())
+                    poke1: data1,
+                    poke2: data2,
+                    poke3: data3,
+                    poke4: data4,
+                    poke5: data5,
+                    poke6: data6,
+                    poke7: data7,
+                    poke8: data8,
+                    poke9: data9
+                }));
     }
 
     render() {
-        const data = this.state.pokeArray;
-        const listItems = data.map((dat) =>
-            <PokemonLogic key={data} pokemon={data}/>
-        )
-        console.log(this.state.pokeArray)
         return (
-            <div className ="pokemon">
-                <div className="pokemon-boxes">
-                    {listItems}
+            <div className = "pokemon">
+                <div className = "pokemon-boxes">
+                    <PokemonLogic pokemon={this.state.poke1}/>
+                    <PokemonLogic pokemon={this.state.poke2}/>
+                    <PokemonLogic pokemon={this.state.poke3}/>
+    
+                    <PokemonLogic pokemon={this.state.poke4}/>
+                    <PokemonLogic pokemon={this.state.poke5}/>
+                    <PokemonLogic pokemon={this.state.poke6}/>
+    
+                    <PokemonLogic pokemon={this.state.poke7}/>
+                    <PokemonLogic pokemon={this.state.poke8}/>
+                    <PokemonLogic pokemon={this.state.poke9}/>
                 </div>
             </div>
         )
