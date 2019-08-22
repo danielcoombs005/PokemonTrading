c30e35cbabbead17378454b95479024a2c4b9901
Daniel Coombs <danielcoombs005@yahoo.com>
2019-08-22 15:00:02 -0400

    updated with sell feature showing on popup

--- pokemontradingreactapp/src/components/PokemonLogic.js
+++ pokemontradingreactapp/src/components/PokemonLogic.js
@@ -9,14 +9,18 @@ class PokemonLogic extends React.Component {
             name: props.pokemon.name,
             type: props.pokemon.type,
             id: props.pokemon.id,
-            showPopUp: false
+            showPopUp: false,
+            buySell: ''
         }
         this.Buy = this.Buy.bind(this);
+        this.BuyBG = this.BuyBG.bind(this);
+        this.Sell = this.Sell.bind(this);
+        this.SellBG = this.SellBG.bind(this);
         this.ShowHidePopUp = this.ShowHidePopUp.bind(this);
     }
 
     Buy(pokemon) {
-        if (this.state.showPopUp) {
+        if (this.state.showPopUp && this.state.buySell === 'buy') {
             return (
                 <div className="popup-outside">
                     <div className="popup">
@@ -31,12 +35,46 @@ class PokemonLogic extends React.Component {
             )
         }
         return (
-            <p></p>
+            null
         )
     }
 
-    async ShowHidePopUp() {
+    async BuyBG() {
+        this.ShowHidePopUp();
         await this.setState({
+            buySell: 'buy'
+        })
+    }
+
+    Sell(pokemon) {
+        if (this.state.showPopUp && this.state.buySell==='sell') {
+            return (
+                <div className="popup-outside">
+                    <div className="popup">
+                        <img id="image" src={this.state.sprite} alt={this.state.name}/>
+                        <h1>Would you like to sell your {this.state.name} for {Math.floor(this.state.id / 2)} coins?</h1>
+                        <div className="pokebuttons">
+                            <button>Ok</button>
+                            <button onClick={this.ShowHidePopUp}>No thanks</button>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        return (
+            null
+        )
+    }
+
+    async SellBG() {
+        this.ShowHidePopUp();
+        await this.setState({
+            buySell: 'sell'
+        })
+    }
+
+    ShowHidePopUp() {
+        this.setState({
             showPopUp: !this.state.showPopUp
         })
     }
@@ -51,11 +89,12 @@ class PokemonLogic extends React.Component {
                     <h3 id="pkmnid">ID: {this.state.id}</h3>
 
                     <div className = "pokebuttons">
-                        <button onClick={this.ShowHidePopUp}>Buy</button>
-                        <button onClick={() => console.log('lol')}>Sell</button>
+                        <button onClick={this.BuyBG}>Buy</button>
+                        <button onClick={this.SellBG}>Sell</button>
                     </div>
                 </div>
                 <this.Buy />
+                <this.Sell />
             </div>
         )
     }
